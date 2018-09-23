742d3ae371297cb78ae3817e4768e54cbf00e588
Mateus Gonçalves <mgm4@cin.ufpe.br>
2018-09-23 14:31:03 -0300

    Added soups of interest to utils. Classification may use it

--- extract.py
+++ extract.py
@@ -2,27 +2,13 @@ import os
 import re
 import urllib.request
 
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup
+
+import utils
+
 
 htmls = [x for x in os.listdir('.') if x.endswith('html')]
 paragraphs = []
 for html in htmls:
-    print('\n----------------------------------------------------------------------------------------------\n', html,':')
-    sauce = open(html, 'r')
-    soup = BS(sauce, 'lxml')
-    desc = re.compile('(description|descricao|informations|features)')
-    divs = list()
-    divs.append(soup.find_all(['div','section'], {'itemprop': desc}))
-    divs.append(soup.find_all(['div','section'], {'id': desc}))
-    divs.append(soup.find_all(['div','section'], {'class': desc}))
-    interest_soup = list()
-    for tag in divs:
-        for div in tag:
-            text = div.text
-            text = text.replace('\n','')
-            if text is not None and not(text in interest_soup):
-                if text.find('R$') == -1:
-                    interest_soup.append(div)
-
-    for i in interest_soup:
-        print(i,'\n---------------------\n')
+    for ip in utils.soups_of_interest(html):
+        print(ip, '\n---------------------\n')
